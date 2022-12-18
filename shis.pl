a44d26a975e26a627cb8ed51bfce23be0f1c05f4
Josh Burns <joshyburnss@gmail.com>
2022-12-19 01:08:18 +1100

    todo list

--- shis.pl
+++ shis.pl
@@ -6,6 +6,16 @@ use warnings;
 use HTTP::CookieJar::LWP ();
 use LWP::UserAgent       ();
 
+# TODO: Build URL
+# - Take @ARGV[0] and check if it starts with schema or not
+# - If it does, do nothing
+# - if it does not, prepend https schema to it
+
+# TODO: Handle (-X / --verb VERB) flag
+# - Will take a positional argument that maps to HTTP verb
+# - get, post, push, patch, put, delete.
+# - Switch case to perform the correct request.
+
 my $jar = HTTP::CookieJar::LWP->new();
 my $ua  = LWP::UserAgent->new(
   cookie_jar        => $jar,
